e601fe38c252e8e187f53f3b0e23d9cc56611eb5
Geovane <geovanegallotticazal@hotmail.com>
2024-08-21 21:01:33 -0300

    Add homebrew install

--- config.sh
+++ config.sh
@@ -2,7 +2,19 @@
 
 DOTFILES=(.gitconfig .zshrc)
 
-for dotfile in $(echo ${DOTFILES[*]});
-do
-    cp ~/dotfiles/$(echo $dotfile) ~/$(echo $dotfile)
+for dotfile in "${DOTFILES[@]}"; do
+    cp ~/dotfiles/$dotfile ~/$dotfile
 done
+
+echo "Downloading and installing Homebrew..."
+
+if ! command -v brew &> /dev/null; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+    echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.zshrc
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+else
+    echo "Homebrew already installed."
+fi
+
+echo "Homebrew installed."
